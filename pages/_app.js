e3fe9f6fb1c649d78505ce729567c6b9d53f432c
NeelPoriya <neelporiya1309@gmail.com>
2023-03-20 21:51:56 +0530

    some more stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
 import "../styles/globals.css";
 import "@fontsource/roboto/300.css";
 import React from "react";
+import { Router } from "next/router";
+import nProgress from "nprogress";
 
 function MyApp({ Component, pageProps }) {
+  // const getLayout = Component.getLayout || ((page) => page);
+  // const component = getLayout(<Component {...pageProps} />);
+  nProgress.configure({
+    showSpinner: false,
+    speed: 200,
+    trickleRate: 0.01,
+  });
+
+  const [loading, setLoading] = React.useState(false);
+  Router.events.on("routeChangeStart", (url) => {
+    nProgress.start();
+    setLoading(true);
+  });
+  Router.events.on("routeChangeComplete", (url) => {
+    nProgress.done();
+    setLoading(false);
+  });
+
   return <Component {...pageProps} />;
 }
 
