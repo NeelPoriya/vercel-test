fbf610f2ef047d3cfba2749894cd8b6a76060e0a
NeelPoriya <neelporiya1309@gmail.com>
2023-03-20 21:48:08 +0530

    some more stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,8 @@
 import "../styles/globals.css";
 import "@fontsource/roboto/300.css";
-import React, { useEffect, useContext } from "react";
-import { ProSidebarProvider } from "react-pro-sidebar";
-import {
-  CssBaseline,
-  getFormLabelUtilityClasses,
-  ThemeProvider,
-} from "@mui/material";
-import theme from "./../config/theme";
-// import AuthContext, { AuthContextProvider } from "./../store/auth-context";
-import MainLayout from "../layouts/mainLayout";
-import { Router } from "next/router";
-import Head from "next/head";
-import nProgress from "nprogress";
+import React from "react";
 
 function MyApp({ Component, pageProps }) {
-  // const getLayout = Component.getLayout || ((page) => page);
-  // const component = getLayout(<Component {...pageProps} />);
-  nProgress.configure({
-    showSpinner: false,
-    speed: 200,
-    trickleRate: 0.01,
-  });
-
-  const [loading, setLoading] = React.useState(false);
-  Router.events.on("routeChangeStart", (url) => {
-    nProgress.start();
-    setLoading(true);
-  });
-  Router.events.on("routeChangeComplete", (url) => {
-    nProgress.done();
-    setLoading(false);
-  });
-
   return <Component {...pageProps} />;
 }
 
